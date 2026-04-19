77cc219219d8206e16cd1143a8f339bb7deb1ca0
ferrles124 <mehmetemredursun9@gmail.com>
2026-04-20 00:21:18 +0300

    Create NPC_CurrentDialogue_Patch.cs

---
+++ src/Patches/NPC_CurrentDialogue_Patch.cs
@@ -0,0 +1,33 @@
+using HarmonyLib;
+using StardewValley;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MedTalk
+{
+    [HarmonyPatch(typeof(NPC), nameof(NPC.CurrentDialogue), MethodType.Getter)]
+    public class NPC_CurrentDialogue_Patch
+    {
+        public static void Postfix(ref NPC __instance, ref Stack<Dialogue> __result)
+        {
+            if (__result == null || __result.Count == 0) return;
+
+            var topDialogue = __result.Peek();
+            if (topDialogue == null || topDialogue.dialogues == null || topDialogue.dialogues.Count == 0) return;
+
+            var firstLine = topDialogue.dialogues[0];
+            if (firstLine == null) return;
+
+            if (firstLine.Text == "[[generate]]")
+            {
+                __result.Pop();
+                AsyncBuilder.Instance.RequestNpcBasic(__instance, "default", "");
+                __result.Clear();
+                return;
+            }
+
+            var lines = topDialogue.dialogues.ToList();
+            DialogueBuilder.Instance.AddDialogueLine(__instance, lines);
+        }
+    }
+}
