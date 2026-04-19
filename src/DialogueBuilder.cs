07e3b7ae91cb652d3b4e1a41571227eb103bcbcc
ferrles124 <mehmetemredursun9@gmail.com>
2026-04-19 19:45:59 +0300

    Delete src/DialogueBuilder.cs

--- src/DialogueBuilder.cs
+++
@@ -1,9 +0,0 @@
-namespace MedTalk
-{
-    public class DialogueBuilder
-    {
-        private static DialogueBuilder _instance;
-        public static DialogueBuilder Instance => _instance ??= new DialogueBuilder();
-        public ModConfig Config { get; set; }
-    }
-}
