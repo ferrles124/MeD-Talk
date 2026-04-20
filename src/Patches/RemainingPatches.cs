683066ecabba3439bb609f5c0d1b9574a69e75dc
ferrles124 <mehmetemredursun9@gmail.com>
2026-04-20 21:38:58 +0300

    Update RemainingPatches.cs

--- src/Patches/RemainingPatches.cs
+++ src/Patches/RemainingPatches.cs
@@ -1,28 +1,4 @@
-using HarmonyLib;
-using StardewValley;
-
 namespace MedTalk
 {
-    [HarmonyPatch(typeof(NPC), nameof(NPC.checkForNewCurrentDialogue))]
-    public class NPC_CheckForNewCurrentDialogue_Patch
-    {
-        public static bool Prefix(NPC __instance, int heartLevel)
-        {
-            if (!DialogueBuilder.Instance.PatchNpc(__instance, 4, true)) return true;
-            AsyncBuilder.Instance.RequestNpcBasic(__instance, $"heartLevel_{heartLevel}", "");
-            return false;
-        }
-    }
-
-    [HarmonyPatch(typeof(NPC), nameof(NPC.GetGiftReaction))]
-    public class NPC_GetGiftReaction_Patch
-    {
-        public static bool Prefix(NPC __instance, ref string __result, StardewValley.Object item)
-        {
-            if (!DialogueBuilder.Instance.PatchNpc(__instance)) return true;
-            AsyncBuilder.Instance.RequestNpcGiftResponse(__instance, item, 0);
-            __result = "";
-            return false;
-        }
-    }
+    // Android uyumluluk için bu patch'ler devre dışı bırakıldı
 }
