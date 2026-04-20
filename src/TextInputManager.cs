b6b2b4c52b9937363d7020fd4b7ba862a37f4155
ferrles124 <mehmetemredursun9@gmail.com>
2026-04-20 22:16:56 +0300

    Update TextInputManager.cs

--- src/TextInputManager.cs
+++ src/TextInputManager.cs
@@ -1,28 +1,33 @@
 using StardewValley;
+using StardewModdingAPI.Events;
 
 namespace MedTalk
 {
     public static class TextInputManager
     {
         private static bool _isProcessing = false;
+        private static int _cooldownTicks = 0;
+        private const int COOLDOWN = 120;
 
-        public static void Initialize() { }
+        public static void Initialize()
+        {
+            ModEntry.SHelper.Events.GameLoop.UpdateTicked += OnUpdateTicked;
+        }
+
+        private static void OnUpdateTicked(object sender, UpdateTickedEventArgs e)
+        {
+            if (_cooldownTicks > 0) _cooldownTicks--;
+            if (_cooldownTicks == 0) _isProcessing = false;
+        }
 
         public static void RequestTextInput(string prompt, NPC npc)
         {
             if (_isProcessing) return;
             if (AsyncBuilder.Instance.AwaitingGeneration) return;
-            
-            _isProcessing = true;
-            AsyncBuilder.Instance.RequestNpcBasic(npc, "touch_input", prompt);
-            
-            ModEntry.SHelper.Events.GameLoop.UpdateTicked += ResetProcessing;
-        }
 
-        private static void ResetProcessing(object sender, StardewModdingAPI.Events.UpdateTickedEventArgs e)
-        {
-            _isProcessing = false;
-            ModEntry.SHelper.Events.GameLoop.UpdateTicked -= ResetProcessing;
+            _isProcessing = true;
+            _cooldownTicks = COOLDOWN;
+            AsyncBuilder.Instance.RequestNpcBasic(npc, "touch_input", "");
         }
     }
 }
