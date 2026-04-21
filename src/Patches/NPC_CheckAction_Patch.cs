63fe405dd32a6587f002e79bf7f6364f64c4d581
ferrles124 <mehmetemredursun9@gmail.com>
2026-04-21 21:24:40 +0300

    Update NPC_CheckAction_Patch.cs

--- src/Patches/NPC_CheckAction_Patch.cs
+++ src/Patches/NPC_CheckAction_Patch.cs
@@ -6,22 +6,24 @@ namespace MedTalk
     [HarmonyPatch(typeof(NPC), nameof(NPC.checkAction))]
     public class NPC_CheckAction_Patch
     {
+        private static int _lastCallTick = -999;
+        private const int MIN_TICKS_BETWEEN_CALLS = 180;
+
         public static bool Prefix(ref NPC __instance, ref bool __result, Farmer who, GameLocation l)
         {
-            Log.Info($"NPC_CheckAction_Patch called for {__instance.Name}");
-
-            if (!who.CanMove)
+            var currentTick = (int)(System.DateTime.Now.Ticks / 166667);
+            
+            if (currentTick - _lastCallTick < MIN_TICKS_BETWEEN_CALLS)
             {
-                Log.Info("Farmer cannot move, skipping");
+                Log.Info($"Cooldown active, skipping");
                 return true;
             }
 
-            if (!DialogueBuilder.Instance.PatchNpc(__instance))
-            {
-                Log.Info("PatchNpc returned false, skipping");
-                return true;
-            }
+            if (!who.CanMove) return true;
+            if (AsyncBuilder.Instance.AwaitingGeneration) return true;
+            if (!DialogueBuilder.Instance.PatchNpc(__instance)) return true;
 
+            _lastCallTick = currentTick;
             Log.Info($"Requesting dialogue for {__instance.Name}");
             DialogueBuilder.Instance.ClearContext();
             TextInputManager.RequestTextInput("", __instance);
