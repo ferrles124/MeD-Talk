c50573837740664dd8bf044e7c1f218bf1a0d6d2
ferrles124 <mehmetemredursun9@gmail.com>
2026-04-21 21:37:12 +0300

    Update NPC_CheckAction_Patch.cs

--- src/Patches/NPC_CheckAction_Patch.cs
+++ src/Patches/NPC_CheckAction_Patch.cs
@@ -6,16 +6,13 @@ namespace MedTalk
     [HarmonyPatch(typeof(NPC), nameof(NPC.checkAction))]
     public class NPC_CheckAction_Patch
     {
-        private static int _lastCallTick = -999;
-        private const int MIN_TICKS_BETWEEN_CALLS = 180;
+        private static bool _isOnCooldown = false;
 
         public static bool Prefix(ref NPC __instance, ref bool __result, Farmer who, GameLocation l)
         {
-            var currentTick = (int)(System.DateTime.Now.Ticks / 166667);
-            
-            if (currentTick - _lastCallTick < MIN_TICKS_BETWEEN_CALLS)
+            if (_isOnCooldown)
             {
-                Log.Info($"Cooldown active, skipping");
+                Log.Info("Cooldown active, skipping");
                 return true;
             }
 
@@ -23,13 +20,28 @@ namespace MedTalk
             if (AsyncBuilder.Instance.AwaitingGeneration) return true;
             if (!DialogueBuilder.Instance.PatchNpc(__instance)) return true;
 
-            _lastCallTick = currentTick;
+            _isOnCooldown = true;
+            ModEntry.SHelper.Events.GameLoop.UpdateTicked += ResetCooldown;
+
             Log.Info($"Requesting dialogue for {__instance.Name}");
             DialogueBuilder.Instance.ClearContext();
             TextInputManager.RequestTextInput("", __instance);
 
             __result = false;
             return false;
+        }
+
+        private static int _tickCount = 0;
+        private static void ResetCooldown(object sender, StardewModdingAPI.Events.UpdateTickedEventArgs e)
+        {
+            _tickCount++;
+            if (_tickCount >= 120)
+            {
+                _tickCount = 0;
+                _isOnCooldown = false;
+                ModEntry.SHelper.Events.GameLoop.UpdateTicked -= ResetCooldown;
+                Log.Info("Cooldown reset");
+            }
         }
     }
 }
