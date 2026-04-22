7a02de0a057f4327225f59c7804fa0e8ebfbd82f
ferrles124 <mehmetemredursun9@gmail.com>
2026-04-22 22:47:43 +0300

    Update NPC_CheckAction_Patch.cs

--- src/Patches/NPC_CheckAction_Patch.cs
+++ src/Patches/NPC_CheckAction_Patch.cs
@@ -6,42 +6,23 @@ namespace MedTalk
     [HarmonyPatch(typeof(NPC), nameof(NPC.checkAction))]
     public class NPC_CheckAction_Patch
     {
-        private static bool _isOnCooldown = false;
-
         public static bool Prefix(ref NPC __instance, ref bool __result, Farmer who, GameLocation l)
         {
-            if (_isOnCooldown)
+            if (__instance.IsInvisible ||
+                !who.CanMove ||
+                !DialogueBuilder.Instance.PatchNpc(__instance))
             {
-                Log.Info("Cooldown active, skipping");
                 return true;
             }
 
-            if (!who.CanMove) return true;
-            if (AsyncBuilder.Instance.AwaitingGeneration) return true;
-            if (!DialogueBuilder.Instance.PatchNpc(__instance)) return true;
-
-            _isOnCooldown = true;
-            ModEntry.SHelper.Events.GameLoop.UpdateTicked += ResetCooldown;
-
-            Log.Info($"Requesting dialogue for {__instance.Name}");
             DialogueBuilder.Instance.ClearContext();
-            TextInputManager.RequestTextInput("", __instance);
+            var character = DialogueBuilder.Instance.GetCharacter(__instance);
+            var prompt = $"What do you want to say to {__instance.displayName}?";
+
+            TextInputManager.RequestTextInput(prompt, __instance);
 
             __result = false;
             return false;
-        }
-
-        private static int _tickCount = 0;
-        private static void ResetCooldown(object sender, StardewModdingAPI.Events.UpdateTickedEventArgs e)
-        {
-            _tickCount++;
-            if (_tickCount >= 120)
-            {
-                _tickCount = 0;
-                _isOnCooldown = false;
-                ModEntry.SHelper.Events.GameLoop.UpdateTicked -= ResetCooldown;
-                Log.Info("Cooldown reset");
-            }
         }
     }
 }
